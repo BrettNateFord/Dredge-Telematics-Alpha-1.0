1b12976e2c16db17875cef9fe9d3de0c75b3b461
BrettNateFord <brettnateford@gmail.com>
2023-12-03 22:44:27 -0500

    Changed to offer basic unrefined graph with notifications

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import socket
 import matplotlib.pyplot as plt
 import re
+import time
 
 #**************************************************************************************************#
 #                                       Data Visualization                                         #
@@ -16,11 +17,11 @@ plt.show()
 # Set up TCP connection to the server
 HOST = '127.0.0.1'  # Loopback IP address
 PORT = 65432  # Port to connect to
-
+low_fuel_notified = False
 # Create lists to store fuel and oil levels for plotting
 fuel_levels = []
 oil_levels = []
-
+t_stamp = 0
 #**************************************************************************************************#
 #                               TCP Connection and Data Display                                    #
 #***************************************************************************************************
@@ -40,18 +41,25 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
         print(f"Received data: {decoded_data}")
         
         # Check if the received data contains fuel and oil levels information
-        if 'Fuel Level' in decoded_data and 'Oil Level' in decoded_data:
+        if 'Fuel Level:' in decoded_data and 'Oil Level:' in decoded_data:
             try:
                 fuel_level = float(re.search(r'\d+\.\d+', decoded_data.split('Fuel Level: ')[1]).group())
                 oil_level = float(re.search(r'\d+\.\d+', decoded_data.split('Oil Level: ')[1]).group())
-                
+                if fuel_level < 55 and not low_fuel_notified:
+                    for i in range(5):
+                        plt.text(t_stamp, fuel_level + i * 5, f"LOW FUEL NOTIFICATION! Fuel at: {fuel_level}%",  color='red')
+
+                        low_fuel_notified = True  # Set the flag to True once the notification is displayed:
+                        plt.annotate('Low Fuel', xy=(time.time(), fuel_level), xytext=(time.time(), fuel_level + 10),
+                            arrowprops=dict(facecolor='red', shrink=0.05))
+                    
                 fuel_levels.append(fuel_level)
+                t_stamp = len(fuel_levels)
                 oil_levels.append(oil_level)
                 
                 # Plot fuel and oil levels in real-time
                 ax.plot(fuel_levels, label='Fuel Level')
                 ax.plot(oil_levels, label='Oil Level')
-                ax.legend()
                 plt.pause(0.01)  # Small pause to update the plot
             except AttributeError:
                 pass  # Ignore non-numeric data
